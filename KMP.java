d30b633500b184306828c2eb3979d55b204e7ee0
Sssumin <81093298+dripdropdr@users.noreply.github.com>
2023-05-22 15:31:01 +0900

    Create KMP.java

---
+++ KMP.java
@@ -0,0 +1,18 @@
+class Result{
+
+    static int KMP(String txt, String pat){
+    
+    
+    }
+    static int patMatchTable(String pat, int M, int fixTable[]){
+        int fixlen = 0;
+        int ptr = 1;
+        fixTable[0] = 0;
+        
+        while(ptr < M){
+          
+        }
+      
+    }
+
+}
