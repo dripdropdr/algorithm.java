39275df0a03b96b69f376726fa7da71bb0eb0c4e
Sssumin <81093298+dripdropdr@users.noreply.github.com>
2023-05-24 13:16:48 +0900

    Create TreePostorder.java

---
+++ TreePostorder.java
@@ -0,0 +1,20 @@
+/* https://www.hackerrank.com/challenges/tree-postorder-traversal/problem?isFullScreen=true
+you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+*/
+
+    public static void postOrder(Node root) {
+        if(root.left != null){
+            postOrder(root.left);
+        }
+        if(root.right != null){
+            postOrder(root.right);
+        } 
+        System.out.print(root.data+" ");
+    }
