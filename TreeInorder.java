9c34d62d226a9d6660b23c17c54be75922c8e6f7
Sssumin <81093298+dripdropdr@users.noreply.github.com>
2023-05-24 13:14:59 +0900

    Create TreeInorder.java

---
+++ TreeInorder.java
@@ -0,0 +1,21 @@
+/* https://www.hackerrank.com/challenges/tree-inorder-traversal/problem?isFullScreen=true
+you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void inOrder(Node root) {
+        if(root.left != null){
+            inOrder(root.left);
+        }
+        System.out.print(root.data+" ");
+        if(root.right != null){
+            inOrder(root.right);
+        }  
+    }
