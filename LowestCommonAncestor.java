b3cba3bfcd96e9f709a4f9a30920ec1495063148
Sssumin <81093298+dripdropdr@users.noreply.github.com>
2023-05-24 10:46:41 +0900

    Create LowestCommonAncestor.java

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,19 @@
+/* https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor/problem?isFullScreen=true
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static Node lca(Node root, int v1, int v2) {
+          if (root == null){
+              return null;
+          }
+          
+          if (root.data > v1 && root.data > v2){
+              return lca(root.left, v1, v2);
+          }
+          if (root.data < v1 && root.data < v2){
+              return lca(root.right, v1, v2);
+          }  
+          return root;  
+    }
